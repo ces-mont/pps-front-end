260d49a6dddbc7c1812b8be13085d3097cef3327
cesar montalvan <ces147@yahoo.com.ar>
2021-10-20 12:10:21 -0300

    add user login

--- src/componentescomunes/formlogin.js
+++ src/componentescomunes/formlogin.js
@@ -1,35 +1,64 @@
-import React,{useState} from 'react';
-import {connect} from 'react-redux';
-import { login, setUser } from '../redux/actions/useractions';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { login, setUser } from "../redux/actions/useractions";
+import { doPreflightCorsPostRequest } from "../apirequests/requests";
 
-const formLogin = ({usuario,loguear,setearUsuario})=>{
-    const[name,setName] = useState('');
-    const[pass,setPass] = useState('');
- 
-    const submitear = (e)=>{
-        e.preventDefault();
-        console.log('submiteando con-> nombre:'+name+' password:'+pass)
-        //{logged:false, token:'', nombre:'',apellido:'',rol:'',idUsuario:''}
-        //loguear({})
-        setearUsuario({token:'apppparapa',nombre:name,apellido:'haceFalta?',rol:'invite',idUsuario:'3444'})
-        console.log('this.props->'+JSON.stringify(usuario))
-    }
+const formLogin = ({ usuario, loguear, setearUsuario }) => {
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
 
-    return(
+  const submitear = (e) => {
+    e.preventDefault();
+    console.log("submiteando con-> nombre:" + name + " password:" + pass);
+    doPreflightCorsPostRequest( "/usuarios/login", JSON.stringify({ nombre: name, contrasenia: pass }), false)
+      .then((rta) => {
+        console.log("post->usuario/login->rta: ", rta);
+        if (rta.logged) {
+          setearUsuario({ token: rta.token, nombre: rta.nombre,
+            apellido: rta.apellido,
+            rol: rta.rol,
+            idUsuario: rta.idUsuario,
+          });
+        }else{
+            //cartel de error en loggeo
+        }
+      })
+      .catch((err) => {
+        console.log("post->usuario/login->err: ", err);
+      });
+    console.log("this.props->" + JSON.stringify(usuario));
+  };
+
+  return (
+    <>
+      {usuario.logged ? (
+        <>
+          <h4>Usted ya esta loggeado</h4>
+        </>
+      ) : (
         <form onSubmit={submitear}>
-            <label className="form-label">Usuario
-                <input type="text" value={name} onChange={()=>setName(event.target.value)} name="usuario" id="idUsuario" className="form-control"/>
-            </label>
-            <br/>
-            <label className="form-label">Contraseña
-                <input type="password" value={pass} onChange={()=>setPass(event.target.value)} name="password" id="idPassword" className="form-control"/>
-            </label>
-            <br/>
-            <button type="submit" className="btn btn-secondary">Ingresar</button>
+          <label className="form-label">
+            Usuario
+            <input type="text" value={name} onChange={() => setName(event.target.value)} name="usuario" id="idUsuario" className="form-control" />
+          </label>
+          <br />
+          <label className="form-label">
+            Contraseña
+            <input type="password" value={pass} onChange={() => setPass(event.target.value)} name="password" id="idPassword" className="form-control"/>
+          </label>
+          <br />
+          <button type="submit" className="btn btn-secondary">
+            Ingresar
+          </button>
         </form>
-    )
-}
+      )}
+    </>
+  );
+};
 
-const mapStateToProps = (state)=>({usuario: state.userReducer});
-const mapFuncToProps = {loguear:()=>login(),setearUsuario:(userData)=>setUser(userData)}
-export default connect(mapStateToProps,mapFuncToProps)(formLogin);
+const mapStateToProps = (state) => ({ usuario: state.userReducer });
+const mapFuncToProps = {
+  loguear: () => login(),
+  setearUsuario: (userData) => setUser(userData),
+};
+export default connect(mapStateToProps, mapFuncToProps)(formLogin);
